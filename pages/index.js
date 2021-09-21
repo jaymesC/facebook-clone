26a2a2034e30d878093ddad7636fbf5ec272bce7
jaymesC <clementjaymes@gmail.com>
2021-09-21 14:01:19 +0100

    adding many files

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import Login from "../components/Login";
 
 export default function Home({ session }) {
-  if (!session) return <Login />
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
