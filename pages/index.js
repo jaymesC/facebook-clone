44265f05367e2977b45e69db1e8891a60aa11f05
jaymesC <clementjaymes@gmail.com>
2021-09-21 12:49:23 +0100

    adding project files

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import { getSession } from "next-auth/client";
+import Head from "next/head";
+import Header from "../components/Header";
+import Login from "../components/Login";
+
+export default function Home({ session }) {
+  if (!session) return <Login />
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+      <Header/>
+      
+
+      {/*head*/}
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* widget */}
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  // Get user
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
+}
+
+
