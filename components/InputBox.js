234dffe6d3e0881656de07861709c2117c1be86b
jaymesC <clementjaymes@gmail.com>
2021-09-22 14:04:25 +0100

    modified inputbox component and globals.css

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,5 +1,7 @@
 import { useSession } from "next-auth/client";
 import Image from "next/image";
+import { EmojiHappyIcon } from "@heroicons/react/outline"
+import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid"
 
 function InputBox() {
     const [session] = useSession();
@@ -22,12 +24,31 @@ function InputBox() {
           />
           <form className="flex flex-1">
             <input
-            className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+              className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
               type="text"
               placeholder={`what's on your mind ${session.user.name}?`}
             />
-            <button hidden type="submit" onClick={sendPost}>Submit</button>
+            <button hidden type="submit" onClick={sendPost}>
+              Submit
+            </button>
           </form>
+        </div>
+
+        <div className="flex justify-evenly p-3 border-t">
+          <div className="inputIcon">
+            <VideoCameraIcon className="h-7 text-red-500" />
+            <p className="text-xs sm:text-sm xl:text-base">Live video </p>
+          </div>
+
+          <div className="inputIcon">
+            <CameraIcon className="h-7 text-green-400" />
+            <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          </div>
+
+          <div className="inputIcon">
+            <EmojiHappyIcon className="h-7 text-yellow-300" />
+            <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity </p>
+          </div>
         </div>
       </div>
     );
