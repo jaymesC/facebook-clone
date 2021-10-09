f2846e26633feabfac29a1549b51077c06c3825a
jaymesC <clementjaymes@gmail.com>
2021-10-09 18:53:06 +0100

    chore(): created and updated Contact component

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,19 @@
-function Contact() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Image from "next/image";
+
+function Contact({ src, name }) {
+  return (
+    <div>
+      <Image 
+      className="rounded-full"
+      objectFit="cover"
+      src={src}
+      width={50}
+      height={50}
+      layout="fixed"
+      alt=""
+      />
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
