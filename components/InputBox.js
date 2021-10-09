d4fdb775400fd6843139bd611a01d3c3e430c091
jaymesC <clementjaymes@gmail.com>
2021-10-09 15:49:32 +0100

    added many components

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,79 +2,124 @@ import { useSession } from "next-auth/client";
 import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
-import { useRef } from "react";
-import { db } from "../firebase";
+import { useRef, useState } from "react";
+import { db, storage } from "../firebase";
 import firebase from "firebase";
 
 function InputBox() {
-    const [session] = useSession();
-    const inputRef = useRef(null);
-    const filePickerRef = useRef(null);
-    const [imageToPost, setimagepost] = useState(null);
+  const [session] = useSession();
+  const inputRef = useRef(null);
+  const filePickerRef = useRef(null);
+  const [imageToPost, setImageTopost] = useState(null);
 
-    const sendPost = (e) => {
-        e.preventDefault();
+  const sendPost = (e) => {
+    e.preventDefault();
 
-        if (!inputRef.current.value) return;
+    if (!inputRef.current.value) return;
 
-        db.collection('posts').add({
-          message: inputRef.current.value,
-          name: session.user.name,
-          email: session.user.email,
-          image: session.user.image,
-          timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        })
-        inputRef.current.value = ''; 
-    };
+    db.collection("posts")
+      .add({
+        message: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then((doc) => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`posts/${doc.id}`)
+            .putString(imageToPost, "data_url");
 
-    const addImageToPost = (e) => {
+          removeImage();
 
+          uploadTask.on("state_change", null, (error) =>
+            console.error(error, () => {
+              // When the upload completes
+            })
+          );
+        }
+      });
+    inputRef.current.value = "";
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
     }
 
-    return (
-      <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
-        <div className="flex space-x-4 p-4 items-center">
-          {/* Profile pic */}
-          <Image
-            className="rounded-full"
-            src={session.user.image}
-            width={40}
-            height={40}
-            layout="fixed"
-            alt="profile pic"
+    reader.onload = (readerEvent) => {
+      setImageTopost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageTopost;
+  };
+
+  return (
+    <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+      <div className="flex space-x-4 p-4 items-center">
+        {/* Profile pic */}
+        <Image
+          className="rounded-full"
+          src={session.user.image}
+          width={40}
+          height={40}
+          layout="fixed"
+          alt="profile pic"
+        />
+        <form className="flex flex-1">
+          <input
+            className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+            type="text"
+            ref={inputRef}
+            placeholder={`what's on your mind ${session.user.name}?`}
           />
-          <form className="flex flex-1">
-            <input
-              className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
-              type="text"
-              ref={inputRef}
-              placeholder={`what's on your mind ${session.user.name}?`}
-            />
-            <button hidden type="submit" onClick={sendPost}>
-              Submit
-            </button>
-          </form>
+          <button hidden type="submit" onClick={sendPost}>
+            Submit
+          </button>
+        </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
+          >
+            <Image className="h-10 object-contain" src={imageToPost} alt="" />
+            <p className="text-xs text-red-500 text-center ">Remove</p>
+          </div>
+        )}
+      </div>
+
+      <div className="flex justify-evenly p-3 border-t">
+        <div className="inputIcon">
+          <VideoCameraIcon className="h-7 text-red-500" />
+          <p className="text-xs sm:text-sm xl:text-base">Live video </p>
         </div>
 
-        <div className="flex justify-evenly p-3 border-t">
-          <div className="inputIcon">
-            <VideoCameraIcon className="h-7 text-red-500" />
-            <p className="text-xs sm:text-sm xl:text-base">Live video </p>
-          </div>
+        <div
+          onClick={() => filePickerRef.current.click()}
+          className="inputIcon"
+        >
+          <CameraIcon className="h-7 text-green-400" />
+          <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          <input
+            ref={filePickerRef}
+            onChange={addImageToPost}
+            type="file"
+            hidden
+          />
+        </div>
 
-          <div className="inputIcon">
-            <CameraIcon className="h-7 text-green-400" />
-            <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
-            <input ref={filePickerRef} onChange={addImageToPost} />
-          </div>
-
-          <div className="inputIcon">
-            <EmojiHappyIcon className="h-7 text-yellow-300" />
-            <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity </p>
-          </div>
+        <div className="inputIcon">
+          <EmojiHappyIcon className="h-7 text-yellow-300" />
+          <p className="text-xs sm:text-sm xl:text-base">Feeling/Activity </p>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default InputBox;
