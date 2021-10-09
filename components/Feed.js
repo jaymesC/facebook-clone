37a3432faa1793f314d5ed4dce62e9daff90e475
jaymesC <clementjaymes@gmail.com>
2021-10-09 18:48:20 +0100

    chore(): updated Feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,15 +1,19 @@
 import Stories from "./Stories"
 import InputBox from "./InputBox"
 import Posts from "./Posts"
+import Contact from "./Contact"
 
 function Feed() {
     return (
-        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
             <div className="mx-auto max-w-md md:max-w-lg">
                 <Stories />
                 <InputBox />
                 <Posts />
             </div>
+            {contacts.map(contact => (
+                <Contact key={contact.src} src={contact.src} name={contact.name} />
+            ))}
         </div>
     )
 }
