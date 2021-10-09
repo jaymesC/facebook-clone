a1eb12a0ea0e9d8726d3a06e1296003eec4338b3
jaymesC <clementjaymes@gmail.com>
2021-10-09 18:42:56 +0100

    chore(): added tailwind plugin

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,3 +1,6 @@
+import {SearchIcon } from "@heroicons/react/outline";
+import { DotsHorizontalIcon, VideoCameraIcon } from "@heroicons/react/solid";
+
 const contacts = [
     { src: "https://links.papareact.com/f0p", name:"Jeff Bezoz"},
     { src: "https://links.papareact.com/kxk", name:"Elon Musk"},
@@ -12,8 +15,15 @@ const contacts = [
 
 function Widgets() {
     return (
-        <div>
-            
+        <div className="hidden lg:flex-col w-60 p-2 mt-5">
+            <div className="flex justify-between items-center text-gray-500 mb-5">
+                <h2 className="text-xl">Contacts</h2>
+                <div className="flex space-x-2">
+                    <VideoCameraIcon className="h-6"/>
+                    <SearchIcon className="h-6"/>
+                    <DotsHorizontalIcon className="h-6"/>
+                </div>
+            </div>
         </div>
     )
 }
